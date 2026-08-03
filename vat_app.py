778b22ca8c465201c6a210607d228d05996c8f04
56964-jao <56964@yupparaj.ac.th>
2026-08-03 13:18:45 +0700

    Fix net price calculation and update display

--- vat_app.py
+++ vat_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - vat
+net_price = price - vatst.divider()
 st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
